72f05348d3a29708f0e2c820b58ad94107db9524
nafisreza <bigganinafis@gmail.com>
2023-10-31 05:30:51 +0600

    ⚡️how to use hooks and propagate state changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(0);
+
+  const increaseValue = () => {
+    setCounter(counter + 1);
+  };
+  const decreaseValue = () => {
+    setCounter(counter - 1);
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h2>React Counter</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          In this project, we are going to learn how to use hooks and propagate
+          state changes.
         </p>
+        <h1>Counter: {counter}</h1>
+        <button className="draw-border" onClick={increaseValue}>Increase</button>
+        <button className="draw-border" onClick={decreaseValue}>Decrease</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
